273263eb6f5c51d3b7a3017d6d9228623b7e498f
Hasibmihad <mdhasibuddin12@gmail.com>
2023-09-07 00:21:58 +0600

    1 st class done

--- PIG_The_Game.py
+++ PIG_The_Game.py
@@ -1,5 +1,22 @@
 import random
+
+
 class Die:
     def roll(self):
         random.seed(0)
         return random.randint(1, 6)
+    
+
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+
+
+    def addScore(self, roll_points):
+        self.score += roll_points
+
+
+class PigTheGame:
+            
