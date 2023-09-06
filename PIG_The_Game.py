80b1394c83d30b04e6452c5ff0d5280aa4011291
Hasibmihad <mdhasibuddin12@gmail.com>
2023-09-07 01:36:01 +0600

    finally done

--- PIG_The_Game.py
+++ PIG_The_Game.py
@@ -4,7 +4,6 @@ import sys
 
 class Dice:
     def roll(self):
-        random.seed(0)
         return random.randint(1, 6)
     
 
@@ -27,48 +26,65 @@ class PigTheGame:
         self.dice = Dice()
         self.current_player = 0
 
-
+    """def is_game_over(self):
+        for player in self.players:
+            if player.score >= 10:
+                return True
+        return False
+    
+"""
     def change_player(self):
         self.current_player = (self.current_player + 1) % len(self.players)
 
     def play_turn(self):
         player = self.players[self.current_player]
         print(f"{player.name}'s turn.")
+        point=0
         while (True):
             roll = self.dice.roll()
             if roll == 1:
                     print(f"{player.name} rolled a 1 and lost his turn.")
-                    player.score = 0
+                    point=0
                     str=player.__str__()
                     print(str)
                     break
                 
             else :
-                    player.addScore(roll)
+                    #player.addScore(roll)
+                    point+=roll
                     print(f"{player.name} rolled a {roll}")
+                    pointstoshow=player.score+point
+                    print (f"{player.name} scored {pointstoshow} points")
+                    #str=player.__str__()
+                    #print(str)
+                    
+                    if pointstoshow>=10:
+                        break
 
-            ch=input("Press r to roll the dice, h to hold : ")
+                     
+            ch=input("Press  'r'  to roll the dice, 'h' to hold : ")
             if ch.lower() == 'h':
                 break
             elif ch.lower() == 'r':
                 continue
             else : 
+                print("\nGame Over!")
                 sys.exit()
+        player.score+=point  
 
-        print(f"{player.name}'s turn is over. {player.name} scored {player.score} points this turn.")
-        self.change_player()    
+        print(f"{player.name}'s turn is over. {player.name} scored {player.score}.")
+        self.change_player()  
 
+  
 
     def play(self):
-        while all(player.score < 100 for player in self.players):
+        while all(player.score < 10 for player in self.players):
             self.play_turn()
 
-
-
-        winners = [player for player in self.players if player.score >= 100]
+        winners = [player for player in self.players if player.score >= 10]
         print("\nGame Over!")
         if len(winners) == 1:
-            print(f"{winners[0].name} is the winner with {winners[0].points} points!")
+            print(f"{winners[0].name} is the winner with {winners[0].score} points!")
 
 
 if __name__ == "__main__":
