931989a484d53b6bb8400bc420afa85b1c3a31aa
Hasibmihad <mdhasibuddin12@gmail.com>
2023-09-07 00:25:30 +0600

    mistake

--- PIG_The_Game.py
+++ PIG_The_Game.py
@@ -17,6 +17,19 @@ class Player:
     def addScore(self, roll_points):
         self.score += roll_points
 
+    def __str__(self):
+        return f"{self.name} ({self.points} points)"
 
 class PigTheGame:
-            
+    def __init__(self, players):
+        self.players = [Player(player_name) for player_name in players]
+        self.currentPlayerIndex = 0
+        self.die = Die()
+
+
+    def switch_player(self):
+        self.current_player_index = (self.current_player_index + 1) % len(self.players)
+
+    def play_turn(self):
+        current_player = self.players[self.current_player_index]
+        print(f"{current_player.name}'s turn.")        
