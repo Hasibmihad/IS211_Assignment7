baec7df985294c484d489373235a8ebbe83894ff
Hasibmihad <mdhasibuddin12@gmail.com>
2023-09-07 00:40:18 +0600

    confused !!!!

--- PIG_The_Game.py
+++ PIG_The_Game.py
@@ -1,7 +1,8 @@
 import random
+import sys
 
 
-class Die:
+class Dice:
     def roll(self):
         random.seed(0)
         return random.randint(1, 6)
@@ -18,18 +19,33 @@ class Player:
         self.score += roll_points
 
     def __str__(self):
-        return f"{self.name} ({self.points} points)"
+        return f"{self.name} ---- > ({self.score} points)"
 
 class PigTheGame:
-    def __init__(self, players):
-        self.players = [Player(player_name) for player_name in players]
-        self.currentPlayerIndex = 0
-        self.die = Die()
+    def __init__(self, num_players):
+        self.players = [Player(input(f"Enter Player {i + 1}'s name: ")) for i in range(num_players)]        self.currentPlayerIndex = 0
+        self.dice = Dice()
+        self.current_player = 0
 
 
-    def switch_player(self):
-        self.current_player_index = (self.current_player_index + 1) % len(self.players)
+    def change_player(self):
+        self.current_player = (self.current_player + 1) % len(self.players)
 
     def play_turn(self):
-        current_player = self.players[self.current_player_index]
-        print(f"{current_player.name}'s turn.")        
+        player = self.players[self.current_player]
+        print(f"{player.name}'s turn.")
+        print (player.__str__)
+        ch=input("Press r to roll the dice, h to hold")
+        if ch.lower()=='r':
+            roll = self.dice.roll()
+            if roll == 1:
+                print(f"{player.name} rolled a 1 and lost his turn.")
+                player.points = 0
+            else :
+                player.addScore(roll)
+        elif ch.lower()=='h':   
+                
+
+
+
+   
